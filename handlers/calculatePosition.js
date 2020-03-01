3c35ea553d2f3c40b9a51f307ef4fce64752551f
scaccoman <scaccoman@gmail.com>
2020-03-01 14:33:11 +0000

    implement mars rover class

--- handlers/calculatePosition.js
+++ handlers/calculatePosition.js
@@ -1,5 +1,6 @@
 'use strict'
 
+const MarsRover = require('../libs/MarsRover')
 const validate = require('../libs/schemas/calculatePosition')
 const responses = require('../libs/responses')
 
@@ -15,9 +16,24 @@ module.exports = async event => {
       throw new Error(JSON.stringify(validate.request.errors))
     }
 
-    console.log('data= ', data)
-    console.log('running')
-    return responses.success('success')
+    const rovers = data.rovers.map(rover => {
+      
+      const instructions = rover.instructions.split('')
+      const marsRover = new MarsRover(rover, data.plateau.gridSize)
+
+      instructions.forEach(instruction => {
+        if (instruction === 'M') return marsRover.move()
+        return marsRover.setDirection(instruction)
+      })
+
+      return {
+        id: marsRover.id,
+        position: marsRover.getPosition(),
+        timestamp: new Date().getTime()
+      }
+    })
+    
+    return responses.success({ rovers })
   } catch (err) {
     return responses.error(err)
   }
