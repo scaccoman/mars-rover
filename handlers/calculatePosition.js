7a01822009f2570461db9cd967d38b2161d82986
scaccoman <scaccoman@gmail.com>
2020-03-01 13:05:25 +0000

    add responses

--- handlers/calculatePosition.js
+++ handlers/calculatePosition.js
@@ -1,12 +1,19 @@
 'use strict'
 
+const responses = require('../libs/responses')
+
 module.exports = async event => {
   try {
+    if (!event || !event.body) {
+      throw new Error('Please provide a payload')
+    }
 
+    const data = JSON.parse(event.body)
+    console.log('data= ', data)
     console.log('running')
-    return { statusCode: 200, body: 'success' }
+    return responses.success('success')
   } catch (err) {
     console.error(err)
-    return { statusCode: 400, body: err.message || err }
+    return responses.error(err)
   }
 }
---
+++ libs/responses.js
@@ -0,0 +1,50 @@
+'use strict';
+
+module.exports = {
+
+  /**
+   * Respond with a 200 success code and message.
+   *
+   * @param data
+   * @param code
+   * @returns
+   */
+  success: (data, code) => {
+
+    if (!data) {
+      throw new Error('I do not have data to return, please supply');
+    }
+
+    return {
+      statusCode: code ? code : 200,
+      headers: {
+        'Access-Control-Allow-Origin': process.env.CORS_URL || '*'
+      },
+      body: JSON.stringify(data)
+    }
+  },
+
+  /**
+   * Response with a generic 400 code or a passed in statusCode.
+   *
+   * @param err
+   * @returns obj
+   */
+  error: err => {
+
+    err = err || null;
+
+    console.error(err);
+
+    return {
+      statusCode: err && err.statusCode ? err.statusCode : 400,
+      headers: {
+        'Access-Control-Allow-Origin': process.env.CORS_URL || '*'
+      },
+      body: JSON.stringify({
+        'message': err && err.message ? err.message : err,
+        'errors': err && err.errors ? err.errors : null
+      })
+    };
+  }
+};
